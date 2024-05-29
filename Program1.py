55d7cad4baba33a66f020bb629b85caad6902aee
Manish9267 <manishkmar9267@gmail.com>
2024-05-29 19:11:35 +0530

    second changes

--- Program1.py
+++ Program1.py
@@ -1,8 +1,8 @@
 from flask import Flask
+helloworld = Flask(__name__)
+@helloworld.route('/')
+def run():
+    return "{\"message\":\"Hey there python\"}"
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+if __name__ == "__main__":
+    helloworld.run(host="0.0.0.0", port=int("3000"), debug=True)
